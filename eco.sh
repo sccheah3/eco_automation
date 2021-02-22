aa72d4de624432f85d0b294233d15b96ef14719f
Stanley Cheah <sccheah3@gmail.com>
2021-02-22 14:56:25 -0800

    test for SUM loaddefaultbioscfg

--- eco.sh
+++ eco.sh
@@ -113,146 +113,184 @@ if [[ $TEST_PMINFO -eq 0 ]]; then
 fi
 
 if [[ $TEST_SUM -eq 0 ]]; then
-	printf "${GREEN} Testing SUM...${NC}\n"
+	printf "${GREEN}Testing SUM...${NC}\n"
 
-	SUM_TESTS=( QueryProductKey GetBIOSInfo GetBmcInfo CheckOOBSupport CheckAssetInfo \
-				CheckSensorData GetDmiInfo GetKcsPriv GetEventLog \
-				ClearEventLog GetEventLog GetPsuInfo GetPowerStatus GetRaidControllerInfo \
-				GetNvmeInfo GetSataInfo GetDefaultBiosCfg )
+	# SUM_TESTS=( QueryProductKey GetBIOSInfo GetBmcInfo CheckOOBSupport CheckAssetInfo \
+	# 			CheckSensorData GetDmiInfo GetKcsPriv GetEventLog \
+	# 			ClearEventLog GetEventLog GetPsuInfo GetPowerStatus GetRaidControllerInfo \
+	# 			GetNvmeInfo GetSataInfo GetDefaultBiosCfg GetCurrentBiosCfg )
 
-	for TEST in ${SUM_TESTS[@]}; do
-		printf "${GREEN} [SUM] $TEST${NC}\n" 
-		echo [SUM - $TEST] >> logs/sum_result.log
-		./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c $TEST | tee -a logs/sum_result.log
-		printf "\n\n" | tee -a logs/sum_result.log
-	done
+	# for TEST in ${SUM_TESTS[@]}; do
+	# 	printf "${GREEN}[SUM] $TEST${NC}\n" 
+	# 	echo [SUM - $TEST] >> logs/sum_result.log
+	# 	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c $TEST | tee -a logs/sum_result.log
+	# 	printf "\n\n" | tee -a logs/sum_result.log
+	# done
 
 
+	# TURN_SYSTEM_OFF
+	# TURN_SYSTEM_ON
+
+	# # set both autopxe and autoipxe commands. need to install TAS on SUT for SUM CheckSystemUtilization
+	# printf "${GREEN}Setting iPXE/PXE command to boot to cburn...${NC}\n\n"
+	# curl -X POST -F command="$CBURN_IMG RC=$HOST_SERV/eco_automate/cburn_TAS_install.sh" -F address=$LAN_MAC -F action=Update -F lega_only=LEGA 172.16.0.3/dct/cburnTools/httpIPXE | tail -n 20
+	# curl -X POST -F command="$CBURN_IMG RC=$HOST_SERV/eco_automate/cburn_TAS_install.sh" -F address=$LAN_MAC -F action=Update -F ipxe_only=IPXE 172.16.0.3/dct/cburnTools/httpIPXE | tail -n 20 
+
+	# printf "${GREEN}Waiting 9 mins to fully boot into cburn and install TAS for CheckSystemUtilization...${NC}\n\n"
+	# sleep 540
+
+	# printf "${GREEN}[SUM] CheckSystemUtilization${NC}\n" 
+	# echo [SUM - CheckSystemUtilization] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c CheckSystemUtilization | tee -a logs/sum_result.log
+	# echo "If you hit an error here, install Thin Agent Service (TAS) on cburn and run the sum command manually." | tee -a logs/sum_result.log
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+	# printf "${GREEN}[SUM] GetMaintenEventLog${NC}\n" 
+	# echo [SUM - GetMaintenEventLog] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c GetMaintenEventLog --st $( date +%Y%m%d -d "5 day ago" ) --et $( date +%Y%m%d ) --count 5 | tee -a logs/sum_result.log
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+	# printf "${GREEN}[SUM] BiosRotManage${NC}\n" 
+	# echo [SUM - BiosRotManage] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c BiosRotManage --action 1| tee -a logs/sum_result.log
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+	# printf "${GREEN}[SUM] BmcRotManage${NC}\n" 
+	# echo [SUM - BmcRotManage] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c BmcRotManage --action 1| tee -a logs/sum_result.log
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+
+	# printf "${GREEN}[SUM] SetPowerAction${NC}\n\n" 
+
+	# # make sure it's in the on state first
+	# TURN_SYSTEM_ON
+
+	# printf "${GREEN}[SUM] SetPowerAction - DOWN${NC}\n" 
+	# echo [SUM - SetPowerAction - DOWN] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 1 | tee -a logs/sum_result.log
+	# sleep 30
+	# POWER_STATUS="$(ipmitool -H $BMC_IP -U $BMC_USER -P $BMC_PASS chassis power status | awk '{print $4}')"
+	# if [[ $POWER_STATUS == off ]]; then
+	# 	printf "${GREEN}SUCCESS${NC} - system is off"
+	# 	echo "SUCCESS - system is off" >> logs/sum_result.log
+	# else
+	# 	printf "${RED}FAIL${NC} - system is still on"
+	# 	echo "FAIL - system is still on" >> logs/sum_result.log
+	# fi
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+
+	# # make sure system is on off state 
+	# TURN_SYSTEM_OFF
+
+	# printf "${GREEN}[SUM] SetPowerAction - UP${NC}\n" 
+	# echo [SUM - SetPowerAction - UP] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 0 | tee -a logs/sum_result.log
+	# sleep 45
+	# POWER_STATUS="$(ipmitool -H $BMC_IP -U $BMC_USER -P $BMC_PASS chassis power status | awk '{print $4}')"
+	# if [[ $POWER_STATUS == on ]]; then
+	# 	printf "${GREEN}SUCCESS${NC} - system is on"
+	# 	echo "SUCCESS - system is on" >> logs/sum_result.log
+	# else
+	# 	printf "${RED}FAIL${NC} - system is still off"
+	# 	echo "FAIL - system is still off" >> logs/sum_result.log
+	# fi
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+
+	# # make sure system is in ON state
+	# TURN_SYSTEM_ON
+
+	# # set both autopxe and autoipxe commands
+	# printf "${GREEN}Setting iPXE/PXE command to boot to cburn...${NC}\n\n"
+	# curl -X POST -F command=$CBURN_IMG -F address=$LAN_MAC -F action=Update -F lega_only=LEGA 172.16.0.3/dct/cburnTools/httpIPXE | tail -n 20
+	# curl -X POST -F command=$CBURN_IMG -F address=$LAN_MAC -F action=Update -F ipxe_only=IPXE 172.16.0.3/dct/cburnTools/httpIPXE | tail -n 20 
+
+	# printf "${GREEN}Waiting 8 mins to fully boot into cburn for SHUTDOWN action...${NC}\n\n"
+	# sleep 480
+
+	# printf "${GREEN}[SUM] SetPowerAction - SOFTSHUTDOWN${NC}\n" 
+	# echo [SUM - SetPowerAction - SOFTSHUTDOWN] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 4 | tee -a logs/sum_result.log
+	# sleep 45
+	# POWER_STATUS="$(ipmitool -H $BMC_IP -U $BMC_USER -P $BMC_PASS chassis power status | awk '{print $4}')"
+	# if [[ $POWER_STATUS == off ]]; then
+	# 	printf "${GREEN}SUCCESS${NC} - system is off"
+	# 	echo "SUCCESS - system is off" >> logs/sum_result.log
+	# else
+	# 	printf "${RED}FAIL${NC} - system is still on Try this manually by booting the SUT to cburn and run the command."
+	# 	echo "FAIL - system is still on. Try this manually by booting the SUT to cburn and run the command." >> logs/sum_result.log
+	# fi
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+
+	# # make sure system is in ON state
+	# TURN_SYSTEM_ON
+
+	# # no good way to confirm these
+	# printf "${RED} May want to check CYCLE, RESET, and REBOOT manually. There is no good way to programmatically confirm success on these commands.${NC}\n\n"
+	# echo "[SUM] May want to check CYCLE, RESET, and REBOOT manually. There is no good way to programmatically confirm success on these commands." >> logs/sum_result.log
+
+	# printf "${GREEN}Waiting 8 mins to fully boot into cburn for REBOOT action...${NC}\n\n"
+	# sleep 480
+
+	# printf "${GREEN}[SUM] SetPowerAction - REBOOT${NC}\n" 
+	# echo [SUM - SetPowerAction - REBOOT] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 5 | tee -a logs/sum_result.log
+	# sleep 30
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+	# printf "${GREEN}[SUM] SetPowerAction - CYCLE${NC}\n" 
+	# echo [SUM - SetPowerAction - CYCLE] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 2 | tee -a logs/sum_result.log
+	# sleep 30
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+	# printf "${GREEN}[SUM] SetPowerAction - RESET${NC}\n" 
+	# echo [SUM - SetPowerAction - RESET] >> logs/sum_result.log
+	# ./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 3 | tee -a logs/sum_result.log
+	# sleep 30
+	# printf "\n\n" | tee -a logs/sum_result.log
+
+
+	printf "${GREEN}[SUM] GetCurrentBiosCfg${NC}\n" 
+	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c GetCurrentBiosCfg | tee temp/currentbioscfg.log 
+	printf "\n\n"
+
+	printf "${GREEN}[SUM] GetDefaultBiosCfg${NC}\n" 
+	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c GetDefaultBiosCfg | tee temp/defaultbioscfg.log
+	printf "\n\n"
+
+	printf "${GREEN}[SUM] LoadDefaultBiosCfg${NC}\n" 
+	echo [SUM - LoadDefaultBiosCfg] >> logs/sum_result.log
+	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c LoadDefaultBiosCfg | tee -a logs/sum_result.log 
 	TURN_SYSTEM_OFF
 	TURN_SYSTEM_ON
+	sleep 360
 
-	# set both autopxe and autoipxe commands
-	printf "${GREEN}Setting iPXE/PXE command to boot to cburn...${NC}\n\n"
-	curl -X POST -F command="$CBURN_IMG RC=$HOST_SERV/eco_automate/cburn_TAS_install.sh" -F address=$LAN_MAC -F action=Update -F lega_only=LEGA 172.16.0.3/dct/cburnTools/httpIPXE | tail -n 20
-	curl -X POST -F command="$CBURN_IMG RC=$HOST_SERV/eco_automate/cburn_TAS_install.sh" -F address=$LAN_MAC -F action=Update -F ipxe_only=IPXE 172.16.0.3/dct/cburnTools/httpIPXE | tail -n 20 
+	printf "${GREEN}[SUM] GetCurrentBiosCfg${NC}\n" 
+	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c GetCurrentBiosCfg | tee temp/loadeddefaultbioscfg.log
+	printf "\n\n"
 
-	printf "${GREEN}Waiting 9 mins to fully boot into cburn and install TAS for CheckSystemUtilization...${NC}\n\n"
-	sleep 540
-
-	printf "${GREEN} [SUM] CheckSystemUtilization${NC}\n" 
-	echo [SUM - CheckSystemUtilization] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c CheckSystemUtilization | tee -a logs/sum_result.log
-	echo "If you hit an error here, install Thin Agent Service (TAS) on cburn and run the sum command manually." | tee -a logs/sum_result.log
-	printf "\n\n" | tee -a logs/sum_result.log
-
-	printf "${GREEN} [SUM] GetMaintenEventLog${NC}\n" 
-	echo [SUM - GetMaintenEventLog] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c GetMaintenEventLog --st $( date +%Y%m%d -d "5 day ago" ) --et $( date +%Y%m%d ) --count 5 | tee -a logs/sum_result.log
-	printf "\n\n" | tee -a logs/sum_result.log
-
-	printf "${GREEN} [SUM] BiosRotManage${NC}\n" 
-	echo [SUM - BiosRotManage] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c BiosRotManage --action 1| tee -a logs/sum_result.log
-	printf "\n\n" | tee -a logs/sum_result.log
-
-	printf "${GREEN} [SUM] BmcRotManage${NC}\n" 
-	echo [SUM - BmcRotManage] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c BmcRotManage --action 1| tee -a logs/sum_result.log
-	printf "\n\n" | tee -a logs/sum_result.log
-
-
-	printf "${GREEN} [SUM] SetPowerAction${NC}\n\n" 
-
-	# make sure it's in the on state first
-	TURN_SYSTEM_ON
-
-	printf "${GREEN} [SUM] SetPowerAction - DOWN${NC}\n" 
-	echo [SUM - SetPowerAction - DOWN] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 1 | tee -a logs/sum_result.log
-	sleep 30
-	POWER_STATUS="$(ipmitool -H $BMC_IP -U $BMC_USER -P $BMC_PASS chassis power status | awk '{print $4}')"
-	if [[ $POWER_STATUS == off ]]; then
-		printf "${GREEN}SUCCESS${NC} - system is off"
-		echo "SUCCESS - system is off" >> logs/sum_result.log
+	# have to output it to a separate file because if we try to put it back to temp/loadeddefaultbioscfg.log, it will be blank
+	tail temp/loadeddefaultbioscfg.log -n $(( $( cat temp/loadeddefaultbioscfg.log | wc -l ) - 7 )) | tee temp/loadeddefaultbioscfg_diff.log
+	tail temp/defaultbioscfg.log -n $(( $( cat temp/defaultbioscfg.log | wc -l ) - 7 )) | tee temp/defaultbioscfg_diff.log
+	
+	diff temp/loadeddefaultbioscfg_diff.log temp/defaultbioscfg_diff.log
+	if [ $? -eq 0 ]; then
+		printf "[${GREEN}SUCCESS${NC} - loaded default bios]\n\n"
+		echo "[SUCCESS - loaded default bios]" >> logs/sum_result.log
 	else
-		printf "${RED}FAIL${NC} - system is still on"
-		echo "FAIL - system is still on" >> logs/sum_result.log
+		printf "[${RED}ERROR${NC} - unable to load default bios]\n\n"
+		echo "[ERROR - unable to load default bios]" | tee -a logs/sum_result.log
 	fi
+
 	printf "\n\n" | tee -a logs/sum_result.log
 
-
-	# make sure system is on off state 
-	TURN_SYSTEM_OFF
-
-	printf "${GREEN} [SUM] SetPowerAction - UP${NC}\n" 
-	echo [SUM - SetPowerAction - UP] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 0 | tee -a logs/sum_result.log
-	sleep 45
-	POWER_STATUS="$(ipmitool -H $BMC_IP -U $BMC_USER -P $BMC_PASS chassis power status | awk '{print $4}')"
-	if [[ $POWER_STATUS == on ]]; then
-		printf "${GREEN}SUCCESS${NC} - system is on"
-		echo "SUCCESS - system is on" >> logs/sum_result.log
-	else
-		printf "${RED}FAIL${NC} - system is still off"
-		echo "FAIL - system is still off" >> logs/sum_result.log
-	fi
-	printf "\n\n" | tee -a logs/sum_result.log
-
-
-	# make sure system is in ON state
-	TURN_SYSTEM_ON
-
-	# set both autopxe and autoipxe commands
-	printf "${GREEN}Setting iPXE/PXE command to boot to cburn...${NC}\n\n"
-	curl -X POST -F command=$CBURN_IMG -F address=$LAN_MAC -F action=Update -F lega_only=LEGA 172.16.0.3/dct/cburnTools/httpIPXE | tail -n 20
-	curl -X POST -F command=$CBURN_IMG -F address=$LAN_MAC -F action=Update -F ipxe_only=IPXE 172.16.0.3/dct/cburnTools/httpIPXE | tail -n 20 
-
-	printf "${GREEN}Waiting 8 mins to fully boot into cburn for SHUTDOWN action...${NC}\n\n"
-	sleep 480
-
-	printf "${GREEN} [SUM] SetPowerAction - SOFTSHUTDOWN${NC}\n" 
-	echo [SUM - SetPowerAction - SOFTSHUTDOWN] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 4 | tee -a logs/sum_result.log
-	sleep 45
-	POWER_STATUS="$(ipmitool -H $BMC_IP -U $BMC_USER -P $BMC_PASS chassis power status | awk '{print $4}')"
-	if [[ $POWER_STATUS == off ]]; then
-		printf "${GREEN}SUCCESS${NC} - system is off"
-		echo "SUCCESS - system is off" >> logs/sum_result.log
-	else
-		printf "${RED}FAIL${NC} - system is still on Try this manually by booting the SUT to cburn and run the command."
-		echo "FAIL - system is still on. Try this manually by booting the SUT to cburn and run the command." >> logs/sum_result.log
-	fi
-	printf "\n\n" | tee -a logs/sum_result.log
-
-
-	# make sure system is in ON state
-	TURN_SYSTEM_ON
-
-
-	# no good way to confirm these
-	printf "${RED} May want to check CYCLE, RESET, and REBOOT manually. There is no good way to programmatically confirm success on these commands.${NC}\n\n"
-	echo "[SUM] May want to check CYCLE, RESET, and REBOOT manually. There is no good way to programmatically confirm success on these commands." >> logs/sum_result.log
-
-	printf "${GREEN}Waiting 8 mins to fully boot into cburn for REBOOT action...${NC}\n\n"
-	sleep 480
-
-	printf "${GREEN} [SUM] SetPowerAction - REBOOT${NC}\n" 
-	echo [SUM - SetPowerAction - REBOOT] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 5 | tee -a logs/sum_result.log
-	sleep 30
-	printf "\n\n" | tee -a logs/sum_result.log
-
-	printf "${GREEN} [SUM] SetPowerAction - CYCLE${NC}\n" 
-	echo [SUM - SetPowerAction - CYCLE] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 2 | tee -a logs/sum_result.log
-	sleep 30
-	printf "\n\n" | tee -a logs/sum_result.log
-
-	printf "${GREEN} [SUM] SetPowerAction - RESET${NC}\n" 
-	echo [SUM - SetPowerAction - RESET] >> logs/sum_result.log
-	./sum*/sum -i $BMC_IP -u $BMC_USER -p $BMC_PASS -c SetPowerAction --action 3 | tee -a logs/sum_result.log
-	sleep 30
-	printf "\n\n" | tee -a logs/sum_result.log
+	#tail temp/currentbioscfg.log -n $(( $( cat temp/currentbioscfg.log | wc -l ) - 7 )) | tee temp/currentbioscfg.log #> temp/currentbioscfg.log
+	# tail temp/defaultbioscfg.log -n $(( $(cat temp/defaultbioscfg.log | wc -l) - 7 )) | tee temp/defaultbioscfg.log #> temp/defaultbioscfg.log
+	# tail temp/loadeddefaultbioscfg.log -n $(( $(cat temp/loadeddefaultbioscfg.log | wc -l) - 7 )) | tee temp/loadeddefaultbioscfg.log #> temp/loadeddefaultbioscfg.log
 
 
 fi
